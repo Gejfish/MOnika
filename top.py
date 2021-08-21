eecc54783187ccc224ba02d0c6d5624efa241146
Gej fish <76708563+Gejfish@users.noreply.github.com>
2021-08-21 04:02:03 +0200

    nic

--- top.py
+++ top.py
@@ -9,7 +9,7 @@ class top(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcwNzk1MjI0MTUzNjkyNTczNiIsImJvdCI6dHJ1ZSwiaWF0IjoxNjA4NDg0NzQwfQ.JvqYqofZpcuUSrLrTsLhRllvxh0nYUdbp0-wtDbNDzo" # set this to your DBL token
+        self.token = "" # set this to your DBL token
         self.topgg = topgg.DBLClient(self.bot, self.token, autopost=True) # Autopost will post your guild count every 30 minutes
 
     async def on_guild_post():
